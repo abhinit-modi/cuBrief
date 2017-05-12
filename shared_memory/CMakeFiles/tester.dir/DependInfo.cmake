9c0984f7505f607d288ab4347a16bf4c49eb4191
abhinit-modi <abhinitmodi@gmail.com>
2017-05-12 19:48:36 -0400

    shared memory

---
+++ shared_memory/CMakeFiles/tester.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/afs/andrew.cmu.edu/usr3/abhinitm/private/15618/15618/newest/dog_serial.cpp" "/afs/andrew.cmu.edu/usr3/abhinitm/private/15618/15618/newest/CMakeFiles/tester.dir/dog_serial.cpp.o"
+  "/afs/andrew.cmu.edu/usr3/abhinitm/private/15618/15618/newest/filter.cpp" "/afs/andrew.cmu.edu/usr3/abhinitm/private/15618/15618/newest/CMakeFiles/tester.dir/filter.cpp.o"
+  "/afs/andrew.cmu.edu/usr3/abhinitm/private/15618/15618/newest/test_dog.cpp" "/afs/andrew.cmu.edu/usr3/abhinitm/private/15618/15618/newest/CMakeFiles/tester.dir/test_dog.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/afs/andrew.cmu.edu/usr3/abhinitm/builds-15618-project/include"
+  "/afs/andrew.cmu.edu/usr3/abhinitm/builds-15618-project/include/opencv"
+  "/usr/local/cuda/include"
+  "include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ shared_memory/CMakeFiles/tester.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tester.dir/./tester_generated_dog_cuda.cu.o"
+  "CMakeFiles/tester.dir/test_dog.cpp.o"
+  "CMakeFiles/tester.dir/dog_serial.cpp.o"
+  "CMakeFiles/tester.dir/filter.cpp.o"
+  "tester.pdb"
+  "tester"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
